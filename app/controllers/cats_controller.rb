2dc779f9c545b32fd3f822438c5ecc22803d2b83
SanjMerchant <sanjmerchant@gmail.com>
2013-09-21 15:08:02 -0400

    Created new page, functional, but redirect to individual entry does not work.

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,6 +1,16 @@
 class CatsController < ApplicationController
 	def index
 		@cats = Cat.all
+	end 
+
+	def create
+		safe_cat = params.require(:cat).permit(:name, :sex, :color, :DOB, :length, :height, :weight, :spayed_neutered, :house_trained, :medical_notes, :human_notes, :other_pet_notes, :description, :adopted)
+		@cat = Cat.new safe_cat
+    if @cat.save
+      redirect_to @cat
+    else
+      render 'new'
+    end
 	end
 
 	def new
